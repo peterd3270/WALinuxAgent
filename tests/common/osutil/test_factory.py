d0812d5c4be3af3b648f12ed4cd3d4ab70f4008a
Peter Duffy <peter@pwduffy.org.uk>
2020-10-29 19:50:47 +0000

    fixed bug in test_factory.py

--- tests/common/osutil/test_factory.py
+++ tests/common/osutil/test_factory.py
@@ -193,7 +193,7 @@ class TestOsUtilFactory(AgentTestCase):
                           distro_code_name="",
                           distro_full_name="",
                           distro_version="3.0")
-        self.assertTrue(type(ret) == DevuanOsUtil)
+        self.assertTrue(type(ret) == DevuanOSUtil)
         self.assertEquals(ret.get_service_name(),"waagent")
 
     def test_get_osutil_it_should_return_redhat(self):
